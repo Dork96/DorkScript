8d52d62be61993762a8229cfd0d22fb626dd52ba
amingnurfalah <48075172+Dork96@users.noreply.github.com>
2021-09-11 03:45:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+RED='\e[1;31m'
+GREEN='\e[0;32m'
+BLUE='\e[0;34m'
+NC='\e[0m
 
 if ["${EUID}" -ne 0]; then
 echo "Jalankan Scrip Dalam Direktori Root Mode"
 exit 1
 fi
-red = '\e[1;31m'
-green = '\e[0;32m'
-NC = '\e[0m'
-
-if [-f "/etc/v2ray/domain"]; then
+[-f "/etc/v2ray/domain"]; then
 echo "Script Sudah Terinstall!!"
 exit 0
 fi
 MYIP=$(wget -qO- icanhazip.com);
-echo -e  "${red}Checking VPS${NC}"
+echo -e  "${RED}Checking VPS${NC}"
 sleep 2
 IZIN=$(curl https://raw.githubusercontent.com/Dork96/rentScript/main/ipvps | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
+echo -e "${GREEN}Permission Accepted...${NC}"
 sleep 2 
 else
 clear
 echo -e ""
 echo -e "======================================="
-echo -e "${red}===[ Permission Denied...!!! ]===${NC}";
+echo -e "${RED}===[ Permission Denied...!!! ]===${NC}";
 echo -e "Contact WA https//wa.me/+6285717614888"
 echo -e "For Registration IP VPS"
 echo -e "======================================="
